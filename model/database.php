a21e167a4849ad8aa687892cafda41dc18caff1a
hgonzalezgaviria <hectorg0g@gmail.com>
2019-03-26 17:23:02 -0500

    Update database.php
    Cambio de conexión para OpenShift

--- model/database.php
+++ model/database.php
@@ -3,7 +3,7 @@ class Database
 {
     public static function StartUp()
     {
-        $pdo = new PDO('mysql:host=localhost;dbname=db_multas;charset=utf8', 'openshift', 'OpenShift.2596');
+        $pdo = new PDO('mysql:host=mysql;dbname=db_multas;charset=utf8', 'root', 'OpenShift.2596');
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
         return $pdo;
     }
